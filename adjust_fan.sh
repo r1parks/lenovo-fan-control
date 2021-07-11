ec117777f0e378366b9ba9c06caf3891a504a08e
Robert Parks <r1parks.ucsd@gmail.com>
2021-07-11 10:22:36 -0400

    reduced loging output and made the SOURCE_DIR value dynamic

--- adjust_fan.sh
+++ adjust_fan.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-
-set -x
-
-SOURCE_DIR=/home/robert/git/lenovo-fan-control
+# SOURCE_DIR=/home/robert/git/lenovo-fan-control
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 while :
 do
-	TEMP="$($SOURCE_DIR/core_temp.sh)"
-	FAN_LEVEL="$($SOURCE_DIR/fan_level.py "$TEMP")"
+	TEMP="$("$SOURCE_DIR"/core_temp.sh)"
+	FAN_LEVEL="$("$SOURCE_DIR"/fan_level.py "$TEMP")"
 	echo "level $FAN_LEVEL" > /proc/acpi/ibm/fan
+	echo "temperature ${TEMP}c, set fan level $FAN_LEVEL"
 	sleep 10
 done
